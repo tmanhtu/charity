4819072ec3dc5ffe460293a3e397b4d493ecd12a
Nguyễn Đăng Cường Quốc <quocndcde160171@fpt.edu.vn>
2023-03-22 16:17:25 +0700

    updatecontact

---
+++ src/java/Contact/ContactService.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Contact;
+
+import java.util.List;
+
+/**
+ *
+ * @author toten
+ */
+public class ContactService {
+    private final ContactDAO  dao = new ContactDAO();
+    
+    public void createMessage(Contact contact) {
+        dao.createMessage(contact);
+    }
+    
+    public List<ContactVO> getAllMessage() {
+        return dao.getAllMessage();
+    }
+}
